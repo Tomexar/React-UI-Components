75cf3a3fd415be8d13cc0aac4cb02d76f98f166a
Tomexar <tommyexar@gmail.com>
2019-04-23 15:34:43 -0600

    moved divs to app.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -9,8 +9,32 @@ const App = () => {
   return (
     <div>
       <CalculatorDisplay />
-      <NumberButton />
-      <ActionButton />
+      <div className ="row1">
+        <button>Clear</button>
+        <button>÷</button>
+      </div>
+      <div className ='row2'>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+      </div>
+      <div className = 'row3'>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>  
+      </div>
+      <div className = 'row4'>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
+      <div className = 'row5'>
+      <button>=</button>
+      <button>0</button>
+      </div>
     </div>
   );
 };
