cf09b229216adf9bcf4f89b523dc47647242c2bf
Tomexar <tommyexar@gmail.com>
2019-04-22 18:33:34 -0600

    fixed files

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () =>{
+    return(
+        <div className = "card-content">
+            <h1>Get started with React</h1>
+            <p class = 'cardp'>React makes it painless to creat interactive UIs. Design simple views for each state in your application.</p>
+            <a href="#">reactjs.org</a>
+        </div>
+    )
+};
+
+export default CardContent;
