40e130f61e8325416dde34759976cf1bf3288e7e
Tomexar <tommyexar@gmail.com>
2019-04-23 14:59:04 -0600

    made the components, might have to change them around

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+const ActionButton = () =>{
+    return (
+        <div className = 'operators'>
+            <div className = 'division'>÷</div>
+            <div className = 'multiplication'>x</div>
+            <div className = 'subtraction'>-</div>
+            <div className = 'addition'>+</div>
+            <div className = 'equals'>=</div>
+        </div>
+    )
+};
+
+export default ActionButton;
+
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+const NumberButton = () =>{
+    return (
+        <div className = 'operators'>
+            <div className = 'row1'>clear</div>
+            <div className = 'row 2'>7 8 9</div>
+            <div className = 'row 3'>4 5 6</div>
+            <div className = 'row 4'>1 2 3</div>
+            <div className = 'row 5'>0</div>
+        </div>
+    )
+};
+
+export default NumberButton;
